acd32e60df593fb8a18f2aa7b87669d401af2cf9
Iann <iconian4@gmail.com>
2025-03-12 16:32:53 +0300

    initialize commit

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased () {
+  return tutorials.map(tutorials => { 
+  return tutorials.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+  });
+
 }
+
+console.log(titleCased())
